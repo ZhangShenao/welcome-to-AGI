c33331a498c6f99f4ce5106f49dd998739276a5d
ZhangShenao <15201440436@163.com>
2025-06-23 18:33:24 +0800

    LangGraph+Tool-Calling

--- LLM应用开发/LangGraph/内容编辑Agent/article_agent.py
+++ LLM应用开发/LangGraph/内容编辑Agent/article_agent.py
@@ -2,11 +2,14 @@
 """
 @Time    : 2025/6/16 15:46 
 @Author  : ZhangShenao 
-@File    : graph.py 
-@Desc    : 构造Graph并执行
+@File    : article_agent.py
+@Desc    : 内容编辑Agent
 """
+
+from IPython.display import Image, display
 from langgraph.constants import START, END
 from langgraph.graph import StateGraph
+from langgraph.graph.state import CompiledStateGraph
 
 from content_node import content_node
 from image_node import image_node
@@ -15,11 +18,10 @@ from summary_node import summary_node
 from title_node import title_node
 
 
-def write_article(topic: str) -> ArticleState:
+def build_agent() -> CompiledStateGraph:
     """
-    撰写文章
-    :param topic: 文章主题
-    :return: 最终生成的文章状态
+    构造Agent
+    :return: 编译好的Agent Graph
     """
 
     # 创建StateGraph图结构
@@ -38,8 +40,18 @@ def write_article(topic: str) -> ArticleState:
     sg.add_edge("summary_node", "image_node")
     sg.add_edge("image_node", END)
 
-    # 编译生成图
+    # 编译Graph并返回
     graph = sg.compile()
+    return graph
+
+
+def write_article(agent: CompiledStateGraph, topic: str) -> ArticleState:
+    """
+    撰写文章
+    :param agent: 内容编辑Agent
+    :param topic: 文章主题
+    :return: 最终生成的文章状态
+    """
 
     # 设置初始状态
     init_state = ArticleState(
@@ -51,7 +63,7 @@ def write_article(topic: str) -> ArticleState:
     )
 
     # 执行Graph,返回生成的最终状态
-    return graph.invoke(init_state)
+    return agent.invoke(init_state)
 
 
 def dump_markdown(state: ArticleState) -> None:
@@ -75,6 +87,25 @@ def dump_markdown(state: ArticleState) -> None:
     print(f"文章已保存至：{title}.md")
 
 
+def show_agent_structure(agent: CompiledStateGraph) -> None:
+    """展示Agent 结构"""
+    display(Image(agent.get_graph(xray=True).draw_mermaid_png()))
+
+    # 保存流程图到文件
+    graph_png = agent.get_graph(xray=True).draw_mermaid_png()
+    with open("agent_graph.png", "wb") as f:
+        f.write(graph_png)
+
+
 if __name__ == '__main__':
-    final_state = write_article("大模型底层的Transformer架构")
+    # 构造Agent
+    agent = build_agent()
+
+    # 展示Agent结构图
+    # show_agent_structure(agent)
+
+    # 写文章
+    final_state = write_article(agent=agent, topic="中国苏超联赛持续火爆")
+
+    # 将文章导出为markdown格式
     dump_markdown(final_state)
--- LLM应用开发/LangGraph/内容编辑Agent/content_node.py
+++ LLM应用开发/LangGraph/内容编辑Agent/content_node.py
@@ -5,13 +5,54 @@
 @File    : title_node.py 
 @Desc    : 文章正文内容节点
 """
+import json
+import os
 
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
+import dotenv
+import requests
+from langchain_core.messages import ToolMessage, SystemMessage, HumanMessage
+from langchain_core.tools import tool
 
 from deepseek import get_deepseek
 from prompt import SYSTEM_PROMPT, CONTENT_PROMPT
 from state import ArticleState
+
+dotenv.load_dotenv()
+
+
+# 定义检索工具
+@tool
+def search(query: str) -> str:
+    """根据关键词，在互联网上检索相关信息
+    Args:
+        query: 检索关键词
+    """
+
+    print(f"调用工具: [search], 调用参数: {query}")
+
+    # 构造请求参数
+    url = "https://google.serper.dev/search"
+    payload = json.dumps({
+        "q": query,
+    })
+    headers = {
+        "X-API-KEY": os.getenv("SERPER_API_KEY"),  # 在Serper官网申请api_key
+        'Content-Type': 'application/json'
+    }
+
+    # 发送请求
+    response = requests.request("POST", url, headers=headers, data=payload).json()
+
+    # 解析响应结果
+    if response['organic'][0]:
+        return response['organic'][0]['snippet']
+
+    return "没有搜索到相关结果"
+
+
+# 定义工具列表
+TOOLS = [search]
+TOOL_DICT = {tool.name: tool for tool in TOOLS}
 
 
 def content_node(state: ArticleState) -> ArticleState:
@@ -30,24 +71,40 @@ def content_node(state: ArticleState) -> ArticleState:
     topic = state["topic"]
     title = state["title"]
 
-    # 构造prompt
-    prompt = ChatPromptTemplate.from_messages([
-        ("system", SYSTEM_PROMPT),
-        ("user", CONTENT_PROMPT)
-    ])
+    # 构造消息列表
+    messages = [
+        SystemMessage(content=SYSTEM_PROMPT.format(topic=topic)),
+        HumanMessage(content=CONTENT_PROMPT.format(title=title)),
+    ]
 
     # 获取DeepSeek客户端
     deepseek = get_deepseek()
+    deepseek = deepseek.bind_tools(TOOLS)
 
-    # 构造Chain
-    chain = prompt | deepseek | StrOutputParser()
+    # 调用DeepSeek,获取结果
+    while True:
+        # 调用LLM,并保存结果
+        reply = deepseek.invoke(messages)
+        messages.append(reply)
 
-    # 调用Chain,获取结果
-    content = chain.invoke({"topic": topic, "title": title})
+        # 未返回工具调用参数,说明模型直接生成了回复,则更新状态后返回
+        if len(reply.tool_calls) == 0:
+            content = reply.content
+            state["content"] = content
+            print(f"文章正文生成完成：共 {len(content)} 字")
+            return state
 
-    # 更新状态
-    state["content"] = content
-    print(f"文章正文生成完成：共 {len(content)} 字")
+        # 解析工具调用参数
+        for tool_call in reply.tool_calls:
+            # 根据名称,获取需要调用的工具
+            tool_name = tool_call["name"]
+            tool = TOOL_DICT[tool_name]
 
-    # 返回处理后的状态
-    return state
+            if tool is not None:
+                # 调用工具,获取结果
+                tool_call_result = tool.invoke(tool_call["args"])
+                print(f"工具调用结果: {tool_call_result}")
+
+                # 保存工具调用结果
+                tool_msg = ToolMessage(content=tool_call_result, tool_call_id=tool_call["id"])
+                messages.append(tool_msg)
